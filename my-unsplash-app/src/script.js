a5a10db2e9e292091b2bdf9e8ce09b3798700fdf
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-04-28 18:26:00 +0530

    Added Weather Using openWeatherApi

--- my-unsplash-app/src/script.js
+++ my-unsplash-app/src/script.js
@@ -8,35 +8,35 @@ fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=${quer
     document.querySelector("#app-body").style.backgroundImage = `url(${data.urls.full})`;
 })
 
-//
-// const WEATHER_KEY = import.meta.env.VITE_WEATHER_KEY;
-//
-//
-//
-//
-// // 1. Get location
-// navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//         const { latitude, longitude } = position.coords;
-//
-//         // 2. Free API endpoint
-//         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_KEY}`)
-//             .then(response => {
-//                 if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 console.log(data);
-//                 const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-//                  document.querySelector("#weather").innerHTML =`
-//                      <img src=${iconUrl} />
-//                     <p>${data.main.temp}</p>
-//                      <p>${data.name}</p>
-//
-//                  `;
-//             })
-//     }
-// );
+
+const WEATHER_KEY = import.meta.env.VITE_WEATHER_KEY;
+
+
+
+
+// 1. Get location
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        const { latitude, longitude } = position.coords;
+
+        // 2. Free API endpoint
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_KEY}`)
+            .then(response => {
+                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+                 document.querySelector("#weather").innerHTML =`
+                     <img src=${iconUrl} />
+                    <p>${data.main.temp}</p>
+                     <p>${data.name}</p>
+
+                 `;
+            })
+    }
+);
 //
 //
 // const now=new Date().getTime();
